2df6eae4ca5227370c347e8862bd12ff78988ae8
Poloznikov Artem <art-clon@mail.ru>
2024-06-15 22:21:32 +0500

    Почистил все лишнее, фундамент готов

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Header from './components/Headers/Header/Header.js';
 import './css/App.css';
 import './css/LightTheme.css'; 
 import './css/DarkTheme.css';
-import ThemeToggleButton from './components/Headers/Header/ThemeToggleButton.js' 
 import RegistrationForm from './components/Authorization/RegistrationForm/RegistrationForm.js';
-import ContentList from './components/Contents/ContentList/ContentList.js';
-import ContentListManga from './components/Contents/ContentList/ContentListManga.js';
-import PageContent from './components/Contents/PageContent/PageContent.js';
 import UserList from './components/User/UserList/UserList.js';
-import MyList from './components/Contents/MyContent/MeList.js';
-import Recommendations from './components/Contents/Recommendations/Recommendations.js';
-import MyListManga from './components/Contents/MyContent/MyListManga.js';
-import RecommendationsManga from './components/Contents/Recommendations/RecommendationsManga.js';
-import BotHeader from './components/Headers/BotHeader/BotHeader.js';
 import LoginForm from './components/Authorization/LoginForm/LoginForm.js';
 import Profile from './components/User/Profile/Profile.js';
 import axios from 'axios';
-import PageContentManga from './components/Contents/PageContent/PageContentManga.js';
 
 function App() {
 
   const [users, setUsers] = useState([
     { id: 1, name: 'Артем', surname: 'Полозников', username: 'Clonn123', password: 'Clonn123', email: 'art-clon@mail.ru', gender: "Мужчина", age: "21" },
-    { id: 2, name: 'Андрей', surname: 'Смирнов', username: 'Gifon', password: 'Gifon', email: 'gifon@mail.ru', gender: "Мужчина", age: "21"  }, 
+    { id: 2, name: 'Дмитрий', surname: 'Сальников', username: 'Lownay', password: 'Lownay', email: 'Lownay@mail.ru', gender: "Мужчина", age: "21"  }, 
   ]);
 
   const [currentUser, setCurrentUser] = useState();
@@ -93,14 +83,6 @@ function App() {
           <Route path="/registration" element={<RegistrationForm />} />
           <Route path="/login" element={<LoginForm users={users} onLogin={handleLogin} />} />
           <Route path="/" element={<UserList users={users} />} />
-          <Route path="/anime/recommendations" element={<Recommendations currentUser={currentUser} />} />
-          <Route path="/myList/:id/:sorttype" element={<MyList currentUser={currentUser} />} />
-          <Route path="/animes/sort/:sorttype" element={<ContentList currentUser={currentUser}/>} />
-          <Route path="/animes/:id" element={<PageContent currentUser={currentUser}/>} />
-          <Route path="/manga/recommendations" element={<RecommendationsManga currentUser={currentUser} />} />
-          <Route path="/myListManga/:id/:sorttype" element={<MyListManga currentUser={currentUser} />} />
-          <Route path="/data-manga/sort/:sorttype" element={<ContentListManga currentUser={currentUser}/>} />
-          <Route path="/mangas/:id" element={<PageContentManga currentUser={currentUser}/>} />
           {currentUser && <Route path="/profile" element={<Profile currentUser={currentUser} onLogout={handleLogout} />} />}
         </Routes>
         </div>   
