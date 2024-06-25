d6bc39fde2dc9e7058503e35c80bf7afdfc46de3
Андрей Пичеев <avpicheev@edu.hse.ru>
2024-06-25 21:12:45 +0500

    Что-то с чем-то

--- frontend/src/components/Calendar/ModalDay.js
+++ frontend/src/components/Calendar/ModalDay.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './ModalDay.css';
 import { addMonths, subMonths, format, startOfDay, startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays, isSameMonth, isSameDay } from 'date-fns';
 import ModalEvent from './ModalEvent';
+import krest from './krest.png';
+import gal from './gal.png';
 
 const ModalDay = ({ isOpen, onClose, events, id_teacher, selectedDate }) => {
     const [isCreateSlotModalOpen, setIsCreateSlotModalOpen] = useState(false);
@@ -18,24 +20,53 @@ const ModalDay = ({ isOpen, onClose, events, id_teacher, selectedDate }) => {
     };
 
     return (
-        <div className="modal-overlay">
-            <div className="modal">
-                <div className="modal-header">
-                    <h2>Слоты на выбранную дату</h2>
-                    <button onClick={openCreateSlotModal}>Добавить слот</button>
-                    <button className="modal-close-btn" onClick={onClose}>Закрыть</button>
-                </div>
-                <div className="modal-body">
-                    <div className="modal-content">
-                        {events.map(event => (
-                            <div key={event.event_id} className="event-item">
-                                <p><strong>Дата:</strong> {event.event_date}</p>
-                                <p><strong>Продолжительность:</strong> {event.duration} минут</p>
-                                {/* Другие поля по необходимости */}
-                            </div>
-                        ))}
+        <div className="modal-overlay">            
+                <div className="modal">
+                    <div className="modal-header">
+                        <h2>Слоты на выбранную дату</h2>
+                        <button onClick={openCreateSlotModal}>Добавить слот</button>
+                        <button className="modal-close-btn" onClick={onClose}>Закрыть</button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="modal-content">
+                            {events.map(event => (
+                                <div key={event.event_id} className="event-item">
+                                    <p><strong>Дата:</strong> {event.event_date}</p>
+                                    <p><strong>Продолжительность:</strong> {event.duration} минут</p>
+                                    {/* Другие поля по необходимости */}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
+                            
+            <div className="modal-container-right">                
+                    <div className="modal-header">
+                        <h2>Заявки на слоты</h2>                        
+                        <button className="modal-close-btn" onClick={onClose}>Закрыть</button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="modal-content">
+                            {events.map(event => (
+                                <div key={event.event_id} className="event-item">
+                                    <p><strong>Дата:</strong> {event.event_date}</p>
+                                    <p><strong>Продолжительность:</strong> {event.duration} минут</p>
+                                    <p><strong>Студент:</strong> Иванов Иван </p>
+                                    <p><strong>Тема:</strong> Консультация </p>
+                                    <div className="event-buttons">
+                                        <button className="event-button">
+                                            <img src={gal} alt="Approve" />
+                                        </button>
+                                        <button className="event-button">
+                                            <img src={krest} alt="Reject" />
+                                        </button>
+                                    </div>
+                                    {/* Другие поля по необходимости */}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                
             </div>
             <ModalEvent 
                 isOpen={isCreateSlotModalOpen} 
@@ -45,6 +76,7 @@ const ModalDay = ({ isOpen, onClose, events, id_teacher, selectedDate }) => {
             />
         </div>
     );
+    
 };
 
 export default ModalDay;
